36403422387a74bda4961c640346445f47beacd3
jtaren <arenjatau@gmail.com>
2025-01-01 12:45:43 +0100

    __init__.py

---
+++ flaskr/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+from models import setup_db, Plant
+from flask_cors import CORS
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
+        return response
+
+    return app
